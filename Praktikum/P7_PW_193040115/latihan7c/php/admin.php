f46e635b33e3abc544f6fa44c15c3edf803caa73
rfqalfauzaan0 <63854782+rfqalfauzaan0@users.noreply.github.com>
2020-05-09 18:44:24 +0700

    p7_pw2020_193040115

---
+++ Praktikum/P7_PW_193040115/latihan7c/php/admin.php
@@ -0,0 +1,82 @@
+<?php 
+
+    session_start();
+
+    if(!isset($_SESSION["username"])) {
+    header("Location: login.php");
+    exit;
+}
+    // menghubungkan dengan file php lainnya
+    require 'functions.php';
+
+    // melakukan query
+    $pakaian = query("SELECT * FROM pakaian");
+
+if(isset($_GET['cari'])) {
+    $keyword = $_GET['keyword'];
+    $pakaian = query("SELECT * FROM pakaian WHERE
+                Foto LIKE '%$keyword%' OR
+                Nama Barang LIKE '%$keyword%' OR
+                Merk Barang LIKE '%$keyword%' OR
+                Official Store Online LIKE '%$keyword%' OR
+                Harga LIKE '%$keyword%'  ");
+} else {
+    $pakaian = query("SELECT * FROM pakaian");
+}
+?>
+
+<html>
+<head>
+    <title>Latihan6a</title>
+    <style>
+        img{
+            width : 150px;
+        }
+    </style>
+</head>
+<body>
+
+  <div class="add">
+    <a href="tambah.php">Tambah data</a>
+  </div>
+  <div class="logout">
+    <a href="logout.php">Logout</a>
+  </div>
+
+   <form action="" method="get">
+        <input type="text" name="keyword" autofocus>
+        <button type="submit" name="cari">Cari</button>
+    </form>
+
+    <table border="1" cellpadding ="13" cellspacing="0">
+        <tr>
+            <th>#</th>
+            <th>opsi</th>
+            <th>Gambar</th>
+            <th>Nama</th>
+            <th>Nama Barang</th>
+            <th>Official Store Online</th>
+            <th>Harga</th>
+        </tr>
+
+        
+        <?php $i = 1; ?>
+        <?php foreach ($pakaian as $pk) : ?>
+            <tr>
+                <td><?= $i; ?></td>
+                <td>
+                    <a href="ubah.php?id=<?= $pk['id'] ?>"><button>Ubah</button></a>
+                    <a href="hapus.php?id=<?= $pk['id']?>" onclick="return confirm('Hapus Data??')"><button>Hapus</button></a>
+                </td>
+                <td><img src="../assets/img/<?= $pk['Foto']?>" alt=""></td>
+                <td><?= $pk['Nama Barang']?></td>
+                <td><?= $pk['Merk barang']?></td>
+                <td><?= $pk['Official Store Online']?></td>
+                <td><?= $pk['Harga']?></td>
+            </tr>
+            <?php $i++;?>
+        <?php endforeach;?>
+    </table>
+
+</body>
+</html>
---
+++ Praktikum/P7_PW_193040115/latihan7c/php/functions.php
@@ -0,0 +1,106 @@
+<?php
+
+//function untuk melakukan koneksi ke database
+function koneksi() {
+    $conn = mysqli_connect("127.0.0.1" , "root" , "") or die("Koneksi ke DB Gagal");
+    mysqli_select_db($conn, "pw_193040115") or die("Database Salah!");
+    
+    return $conn;
+}
+
+//function untuk melakukan query ke database
+function query($sql){
+    $conn = koneksi();
+    $result = mysqli_query($conn, "$sql");
+
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+
+    return $rows;
+}
+
+//functions untuk menambahkan data didalam database
+function tambah($data) {
+    $conn = koneksi();
+
+     $id           = $data['id'];
+    $foto         = htmlspecialchars($data['Foto']);
+    $nama         = htmlspecialchars($data['Nama Barang']);
+    $merk         = htmlspecialchars($data['Merek Barang']);
+    $official     = htmlspecialchars($data['Official Store Online']);
+    $harga        = htmlspecialchars($data['Harga']);
+
+
+    $query = "INSERT INTO pakaian
+                VALUES 
+                ('','$foto','$nama','$merk','$official' , '$harga')";
+    
+    mysqli_query($conn , $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+//functions untuk menghapus data 
+function hapus($id){
+    $conn = koneksi();
+    mysqli_query($conn, "DELETE FROM pakaian WHERE id = $id");
+
+    return mysqli_affected_rows($conn);
+}
+
+//functions untuk menambahkan data didalam database
+function ubah($data) {
+    $conn = koneksi();
+
+    $id           = $data['id'];
+    $foto         = htmlspecialchars($data['Foto']);
+    $nama         = htmlspecialchars($data['Nama Barang']);
+    $merk         = htmlspecialchars($data['Merek Barang']);
+    $official     = htmlspecialchars($data['Official Store Online']);
+    $harga        = htmlspecialchars($data['Harga']);
+
+   $query = "UPDATE pakaian SET
+            Foto =$foto
+            Nama Barang = $nama
+            Merk Barang = $merk
+            Official Store Online = $offical
+            Harga = $harga
+            where id =$id";
+
+    
+    mysqli_query($conn , $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+// fungctions untuk registrasi
+function registrasi($data){
+    $conn = koneksi();
+    $username = strtolower(stripcslashes($data["username"]));
+    $password = mysqli_real_escape_string($conn, $data["password"]);
+
+    // cek username sudah ada atau belum
+    $result = mysqli_query($conn, "SELECT username FROM user WHERE username = '$username' ");
+    if(mysqli_fetch_assoc($result)) {
+        echo "
+        <script>
+            alert('username sudah digunakan');
+        </script>";
+        return false;
+    }
+
+    //enkripsi password
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    // tambah user baru
+    $query_tambah = "INSERT INTO user VALUES ('' , '$username' , '$password')";
+    mysqli_query($conn, $query_tambah);
+
+    return mysqli_affected_rows($conn);
+}
+
+
+
+?>
